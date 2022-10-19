4877a230bcd4157bb72a45909287b0d5accf6a66
Precup Eduard <precupeduard99@gmail.com>
2022-10-19 15:35:31 +0300

    Update - Create login/register page container

--- application_base/client/js/main.js
+++ application_base/client/js/main.js
@@ -9,6 +9,21 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3500,
+        autoplaySpeed: 2500,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 900,
+            settings: {
+              slidesToShow: 2
+            }
+          },
+          {
+            breakpoint: 520,
+            settings: {
+              slidesToShow: 1
+            }
+          }
+        ]
       });
 });
