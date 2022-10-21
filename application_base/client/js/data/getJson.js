59b7b819cd8847f11f510e3ec6ced996aeba9a32
Precup Eduard <precupeduard99@gmail.com>
2022-10-21 10:23:49 +0300

    New function, rendering objects

--- application_base/client/js/data/getJson.js
+++ application_base/client/js/data/getJson.js
@@ -1,7 +1,6 @@
 const getJSON = function() {
 
     let array = [];
-    let hotelList = $('.hotels-list');
 
     $.getJSON('data/hotels/hotels.json', (hotels) => {
         // console.log(hotels)
@@ -16,22 +15,34 @@ const getJSON = function() {
         // for (let i = 0; i < carousel.length; i++) {
         //     carousel[i].innerHTML += hotels[i].name
         // }
+        
 
-        // for ( let i = 0; i < hotels.length; i++) {
-        //     hotelList.html(`
-        //     <li class="hotels-list__item">
-        //         <h2>${hotels[i].name}</h2>
-        //         <p>${hotels[i].stars}</p>
-        //         <p>${hotels[i].location}</p>
-        //         <img src="${hotels[i].image1}" alt="Hotel ${hotels[i].name}">
-        //     </li>
-        //     `)
-        // }
-        console.log(typeof hotels)
-        $.each(hotels, (index, key) => {
-            console.log(hotels[index].name)
-        })
+        for ( let i = 0; i < hotels.length; i++) {
+            
+            let hotelList = document.querySelector('.hotels-list');  
+            hotelList.innerHTML += `
+            <li class="hotels-list__item">
+                <h2>${hotels[i].name}</h2>
+                <h3>${hotels[i].stars}</h3>
+                <h3>${hotels[i].location}</h3>
+                <img src="${hotels[i].image1}" alt="Hotel ${hotels[i].name}">
+            </li>
+            `
 
+            console.log(hotels[i].name)
+        }
+
+        // console.log(typeof hotels)
+        // $.each(hotels, (index, key) => {
+        //     // console.log(hotels[index].name)
+
+            
+        // hotelList.html(`
+        // <li class="hotels-list-item">
+        //     <p>${hotels.}</p>
+        // </li>
+        // `)
+        // })
     })
 }
 
