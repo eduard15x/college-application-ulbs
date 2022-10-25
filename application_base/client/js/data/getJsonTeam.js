4dab8005aa4bd90fa64e1108b7c4a6e7042ff3f4
Precup Eduard <precupeduard99@gmail.com>
2022-10-25 11:58:14 +0300

    Update project - bring data from team.json and finish contact section

--- application_base/client/js/data/getJsonTeam.js
+++ application_base/client/js/data/getJsonTeam.js
@@ -1,25 +1,27 @@
 const getJSONTeam = function() {
 
     let teamList = $('.container-contact-list')
-    
+    let teamMemberArray = ''
+
     $.getJSON('data/team/team.json', (team) => {
         console.log(team)
         console.log(team[1])
+        teamMemberArray = team
+        console.log(typeof(teamMemberArray))
+        console.log(teamMemberArray)
 
-        // team.each(function(index) {
-        //     teamList.html(
-        //         `
-        //         <li>Name ${team[index].name}</li>
-        //         `
-        //     )
-        // })
-
-        $.each(team, function() {
-            $.each(this, function(index, value) {
-                teamList.html(`
-                <li>Name ${this.index}</li>
-                `)
-            })
+        $.each(team, function(key, value) {
+            teamList.append(
+            `<li class="container-contact-list__item">
+                <img src="${value.img}" alt="${value.name}" class="container-contact-list__item--img">
+                <p class="container-contact-list__item--text">${value.name}</p>
+                <p class="container-contact-list__item--text">${value.role}</p>
+                <span class="container-contact-list__item--span"> 
+                    <a href="mailto:${value.mail}"><i class="fa-regular fa-envelope"></i></a> 
+                    <a href="tel:${value.tel}"><i class="fa-solid fa-phone-volume"></i></a> 
+                </span>
+            </li>`)
+                
         })
     })
 
---
+++ application_base/client/js/nav/showPage.js
@@ -0,0 +1,3 @@
+let showPage = function() {
+    
+}
